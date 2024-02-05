c3311463f520f9fa1bafdb14905edeeb756478a0
Adilson <usadilsondigital@gmail.com>
2024-02-05 08:47:47 -0600

    adding references to system xml linq

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
-﻿namespace ConsoleAppXmlExamples
+﻿using System;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ConsoleAppXmlExamples
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Hello, World!");
         }
     }
